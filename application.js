cd6752c67f8c787ef8b18b2ec926e12363903e3a
shravanakumar.gh@tayana.in <shravanakumar.gh@tayana.in>
2021-08-10 13:36:23 +0530

    updated code

--- application.js
+++ application.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/sample', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello shravanakumar!')
 })
 
 const port = process.env.PORT || '5000';

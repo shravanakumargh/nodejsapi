fdbf624f49ba2835de58b8d9c3867f01f38c0867
shravanakumar.gh@tayana.in <shravanakumar.gh@tayana.in>
2021-08-10 14:49:46 +0530

    added post

--- application.js
+++ application.js
@@ -4,6 +4,8 @@ const app = express()
 app.get('/sample', (req, res) => {
   res.send('Hello shravanakumar!')
 })
-
+app.post('/upload', (req, res) => {
+  res.send('Hello shravanakumar!')
+})
 const port = process.env.PORT || '5000';
 app.listen(port, () => console.log(`Server started on Port ${port}`));
